e866e109608b4846fdda3d0a0e4364333ef40832
Olga Illarionova <myaumyx@yandex.ru>
2015-09-12 15:58:07 -0700

    plot12

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 ##read
-#electricity <- read.csv("household_power_consumption.txt", sep = ";",na.strings = "?")
+electricity <- read.csv("household_power_consumption.txt", sep = ";",na.strings = "?")
 ## filter 2007-02-01 2007-02-02
 electr <- electricity[electricity$Date == '1/2/2007' | electricity$Date == '2/2/2007' ,]
 png("plot1.png",width = 480, height = 480, units = "px")
